fdae4c8ade14894ed7b29d19d504e1d68d39d08f
MattFoley <tfallon@mail.depaul.edu>
2013-04-17 14:24:22 -0500

    Update NSString+USStateMap.h

--- Classes/NSString+USStateMap.h
+++ Classes/NSString+USStateMap.h
@@ -6,6 +6,7 @@
 #import <Foundation/Foundation.h>
 
 @interface NSString (USStateMap)
++ (NSDictionary *) stateAbbreviationsMap;
 - (NSString*) stateAbbreviationFromFullName;
 - (NSString*) stateFullNameFromAbbreviation;
 @end
